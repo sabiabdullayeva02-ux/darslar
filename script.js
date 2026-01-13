37aa83398a1efe4a764ed45c0df8d6609702abbd
sabiabdullayeva02-ux <sabiabdullayeva02@gmail.com>
2026-01-13 17:14:13 +0500

    Kalkulator

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('test');
+const son1 = 10;
+const son2 = 5;
+const arifmetikAmal = "*";
+let natija;
+
+switch (arifmetikAmal) {
+    case "+":
+        natija = son1 + son2;
+        break;
+    case "-":
+        natija = son1 - son2;
+    case "*":
+        natija = son1 * son2;
+        break;
+    case "/":
+        natija = son1 / son2;
+        break;
+    default:
+        natija = "arifmetikAmal topilmadi";
+        break;
+}
+console.log (natija);
+
+
+
